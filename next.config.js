92867f91b148eeeb6b74f0a5f3bbadaa946c856b
alkeops <m_adan2012@outlook.com>
2021-06-19 23:03:58 -0500

    Images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   images: {
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     domains: [process.env.WORDPRESS_URL],
+    path: '/_next/image',
+    loader: 'default',
   },
     webpack(config) {
       config.module.rules.push({
