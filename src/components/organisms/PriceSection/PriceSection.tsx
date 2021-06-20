0d5e613f2fa7809ee3d93048b85a4ca0d01687f0
alkeops <m_adan2012@outlook.com>
2021-06-20 10:39:50 -0500

    Precios

--- src/components/organisms/PriceSection/PriceSection.tsx
+++ src/components/organisms/PriceSection/PriceSection.tsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Group from "@assets/illustrations/group.svg";
-import { Title } from "@atoms";
-import {Circle} from "@molecules";
+import { Title, Button } from "@atoms";
+import { Circle } from "@molecules";
 import { PriceTable } from "@organisms";
 const PriceSection = () => {
   const prefix: string = "o-price-section";
   return (
     <section className={prefix}>
       <div className={`${prefix}__header`}>
-        <Title>
-          ¿Qué esperas para <span>organizar</span> tus productos?
-        </Title>
+        <div>
+          <Title>
+            ¿Qué esperas para <span>organizar</span> tus productos?
+          </Title>
+          <Button content="Empezar ahora" />
+        </div>
         <Group className={`${prefix}__header-group`} />
       </div>
       <div className={`${prefix}__tables`}>
-        <PriceTable title="Starter" isStarter />
-        <PriceTable title="Enterprise" />
+        <div className={`${prefix}__tables-align`}>
+          {" "}
+          <PriceTable title="Entrepreneur" isEntrepreneur />
+        </div>
+        <div className={`${prefix}__tables-align`}>
+          <PriceTable title="Seller" isStarter />{" "}
+        </div>
+        <div className={`${prefix}__tables-align`}>
+          <PriceTable title="Enterprise" />
+        </div>
       </div>
       <Circle className={`${prefix}__circle`} />
     </section>
--- src/components/organisms/PriceTable/PriceTable.tsx
+++ src/components/organisms/PriceTable/PriceTable.tsx
@@ -6,14 +6,16 @@ import { PricePoint } from "@molecules";
 const PriceTable = ({
   title,
   isStarter,
+  isEntrepreneur,
 }: {
   title: string;
   isStarter?: boolean;
+  isEntrepreneur?: boolean;
 }) => {
   const prefix: string = "o-price-table";
   const classForComponent: string = classNames(`${prefix}`, {
-    "is-starter": isStarter,
-    "is-enterprise": !isStarter,
+    "is-starter": isStarter || isEntrepreneur,
+    "is-enterprise": !isStarter && !isEntrepreneur,
   });
   return (
     <div className={classForComponent} data-aos="fade-up">
@@ -21,21 +23,29 @@ const PriceTable = ({
         <h3 className={`${prefix}__title`}>{title}</h3>
         {isStarter ? (
           <div>
-            <PricePoint isStarter content="15 canales de venta" />
+            <PricePoint isStarter content="Hasta 15 canales de venta" />
             <PricePoint isStarter content="Hasta 500 SKU's" />
             <PricePoint isStarter content="1 Usuario admin" />
             <PricePoint isStarter content="5 Usuarios consulta" />
             <PricePoint isStarter content="150gb Almacenamiento" />
           </div>
+        ) : isEntrepreneur ? (
+          <div>
+            <PricePoint isStarter content="Hasta 3 canales de venta" />
+            <PricePoint isStarter content="Hasta 100 SKU's" />
+            <PricePoint isStarter content="1 Usuario admin" />
+            <PricePoint isStarter content="1 Usuarios consulta" />
+            <PricePoint isStarter content="50gb Almacenamiento" />
+          </div>
         ) : (
           <div>
-            <PricePoint content="25 canales de venta" />
+            <PricePoint content="Hasta 25 canales de venta" />
             <PricePoint content="Hasta 1500 SKU's" />
             <PricePoint content="2 Usuarios admin" />
             <PricePoint content="10 Usuarios consulta" />
             <PricePoint content="1 TB Almacenamiento" />
             <PricePoint content="Servidor dedicado" />
-            <PricePoint content="Atencion prioritaria" />
+            <PricePoint content="Atención prioritaria" />
           </div>
         )}
       </div>
@@ -43,14 +53,28 @@ const PriceTable = ({
       {isStarter && (
         <>
           <h3 className={`${prefix}__price`}>
-            $599<span>usd</span>
+            $799<span>usd</span>
           </h3>
           <h4 className={`${prefix}__mensual`}>Mensual</h4>
         </>
       )}
-      <div className={!isStarter && `${prefix}__container`}>
+      {isEntrepreneur && (
+        <>
+          <h3 className={`${prefix}__price`}>
+            $299<span>usd</span>
+          </h3>
+          <h4 className={`${prefix}__mensual`}>Mensual</h4>
+        </>
+      )}
+      <span className={`${prefix}__foto`}>
+        ¡Más de {isStarter ? "25,000" : isEntrepreneur ? "1,500" : "150,000"}{" "}
+        Fotografías al mes!
+      </span>
+      <div className={!isStarter && !isEntrepreneur && `${prefix}__container`}>
         <Button
-          content={isStarter ? "Comienza Ahora" : "Contactanos"}
+          content={
+            isStarter || isEntrepreneur ? "Comienza Ahora" : "Contactanos"
+          }
           className={`${prefix}__button`}
         />
       </div>
