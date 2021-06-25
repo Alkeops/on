88572490e3d00fb42921c598866d9bb280b3ace1
alkeops <m_adan2012@outlook.com>
2021-06-25 14:15:13 -0500

    Form

--- src/components/organisms/FormLead/FormLead.tsx
+++ src/components/organisms/FormLead/FormLead.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
-
+import Checkbox from '@material-ui/core/Checkbox';
+import {Button }from "@atoms";
 const options: Array<string> = [
   "Busqueda por internet",
   "Google",
@@ -12,20 +13,46 @@ const options: Array<string> = [
 ];
 
 const FormLead = () => {
+  const prefix: string = "o-form-lead";
+
   return (
-    <form noValidate autoComplete="off">
-      <TextField label="Correo Electronico corporativo" fullWidth />
-      <TextField label="Nombre " fullWidth />
-      <TextField label="Apellidos" fullWidth />
-      <TextField label="Compañia" fullWidth />
-      <TextField label="Teléfono" fullWidth />
-      <TextField label="Como te enteraste de Nubesk" fullWidth select>
+    <form
+      noValidate
+      autoComplete="off"
+      className={prefix}
+      onSubmit={(e) => e.preventDefault()}
+    >
+      <TextField
+        required
+        label="Correo Electronico corporativo"
+        fullWidth
+        className={`${prefix}__input`}
+      />
+      <TextField
+        required
+        label="Nombre "
+        fullWidth
+        className={`${prefix}__input`}
+      />
+      <TextField label="Apellidos" fullWidth className={`${prefix}__input`} />
+      <TextField label="Compañia" fullWidth className={`${prefix}__input`} />
+      <TextField label="Teléfono" fullWidth className={`${prefix}__input`} />
+      <TextField
+        required
+        label="Como te enteraste de Nubesk"
+        fullWidth
+        select
+        className={`${prefix}__input`}
+      >
         {options.map((option) => (
           <MenuItem key={option} value={option}>
             {option}
           </MenuItem>
         ))}
       </TextField>
+      <div className={`${prefix}__check`}><Checkbox color="primary" /> <span>Acepto la Política de Privacidad</span></div>
+      
+      <Button content="Descargar" disabled={true}/>
     </form>
   );
 };
