042009162a7a133b2de5cb04484edc17db4af7ab
alkeops <m_adan2012@outlook.com>
2021-06-29 10:23:57 -0500

    TEst

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -18,9 +18,9 @@ const posts = ({ post, posts, preview }) => {
   return (
     <UserLayout>
       <Head>
-        <meta name="og:title" content={postInfo.title} />
+        <meta name="og:title" content={postInfo.title || "Un post interesante"} />
         <title>Nubesk no se detiene </title>
-        <meta name="og:description" content={postInfo.excerpt} />
+        <meta name="og:description" content={postInfo.excerpt || "Visto en Nubesk"} />
       </Head>
       <img
         className={`${prefix}__image`}
