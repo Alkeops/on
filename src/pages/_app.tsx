894181c5648fb29f1bc1d93a8491d464ee231d63
alkeops <m_adan2012@outlook.com>
2021-06-29 18:17:45 -0500

    Relaunch

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,6 @@ import { useEffect } from "react";
 import "aos/dist/aos.css";
 import "scss/index.scss";
 import { AppProvider } from "@contexts/AppContext";
-import Router from "next/router";
-import NProgress from "nprogress"; //nprogress module
-import "nprogress/nprogress.css"; //styles of nprogress
-
-/* //Binding events.
-Router.events.on("routeChangeStart", () => NProgress.start());
-Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done()); */
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
