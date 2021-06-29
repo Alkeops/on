8fb6d94ebe5a0ab66db57bd902c0f5156350ce45
alkeops <m_adan2012@outlook.com>
2021-06-29 18:14:09 -0500

    Social

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,10 +8,10 @@ import Router from "next/router";
 import NProgress from "nprogress"; //nprogress module
 import "nprogress/nprogress.css"; //styles of nprogress
 
-//Binding events.
+/* //Binding events.
 Router.events.on("routeChangeStart", () => NProgress.start());
 Router.events.on("routeChangeComplete", () => NProgress.done());
-Router.events.on("routeChangeError", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done()); */
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
